e7fe9619ee4f2d4acce2f65456b9961d4977f066
modjordan <mod.jordan@icloud.com>
2023-06-21 12:45:43 -0700

    corrected function, updated readme and removed console.log statements

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Hello! Welcome to my Promgramming Language Quiz!
-**Contributors:** Jordan Kennedy
-## This is a webpage containing information about me and my projects
-**Application:** HMTL, CSS, Bootstrap, Javascript
-**Link to site on GitHub:** https://github.com/modjordan/Portfolio.git
-**Technologies used:** Virtual Studio Code
-**Setup Instructions:** Download all images and files, make sure images and css files are in correct parent folders, deploy.
-**Known Bugs:** None.
+
+# _Programming Quiz_
+
+#### By _**Jordan Kennedy**_
+
+#### _{The application takes user slected answers and outputs a programming language based on the answers}_
+
+## Technologies Used
+
+* HTML
+* JavaScript
+* CSS
+* _here_
+
+## Description
+
+_{Download all images and files, make sure images and css files are in correct parent folders, deploy.}_
+
+## Setup/Installation Requirements
+
+* Clone or Fork project
+* Make sure all images and files are in the proper folders
+* deploy
+
+## Known Bugs
+
+* None
+
+## License
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
 **License:** Apache License 2.0; June 17th, 2023
--- js/script.js
+++ js/script.js
@@ -10,60 +10,56 @@ window.onload = function () {
     // User Input functions
     function q1() {
       if (q1Select === "Python1") {
-        q1Select = parseInt(10);
+        q1Select = 10;
       } else if (q1Select === "Java1") {
-        q1Select = parseInt(-10);
+        q1Select = -10;
       } else if (q1Select === "cSharp1") {
-        q1Select = parseInt(100);
+        q1Select = 100;
       } return q1Select;
     }
     function q2() {
       if (q2Select === "Python2") {
-        q2Select = parseInt(10);
+        q2Select = 10;
       } else if (q2Select === "Java2") {
-        q2Select = parseInt(-10);
+        q2Select = -10;
       } else if (q2Select === "cSharp2") {
-        q2Select = parseInt(100);
-        console.log(q2Select);
+        q2Select = 100;
       } return q2Select;
     }
     function q3() {
       if (q3Select === "Python3") {
-        q3Select = parseInt(10);
+        q3Select = 10;
       } else if (q3Select === "Java3") {
-        q3Select = parseInt(-10);
+        q3Select = -10;
       } else if (q3Select === "cSharp3") {
-        q3Select = parseInt(100);
-        console.log(q3Select);
+        q3Select = 100;
       } return q3Select;
     }
     function q4() {
       if (q4Select === "Python4") {
-        q4Select = parseInt(10);
+        q4Select = 10;
       } else if (q4Select === "Java4") {
-        q4Select = parseInt(-10);
+        q4Select = -10;
       } else if (q4Select === "cSharp4") {
-        q4Select = parseInt(100);
-        console.log(q4Select);
+        q4Select = 100;
       } return q4Select;
     }
     function q5() {
       if (q5Select === "Python5") {
-        q5Select = parseInt(10);
+        q5Select = 10;
       } else if (q5Select === "Java5") {
-        q5Select = parseInt(-10);
+        q5Select = -10;
       } else if (q5Select === "cSharp5") {
-        q5Select = parseInt(100);
-        console.log(q5Select);
+        q5Select = 100;
       } return q5Select;
     }
     let result = function submitAnswers() {
       // Submission function to calculate results
-      let r1 = q1(q1Select);
-      let r2 = q2(q2Select);
-      let r3 = q3(q3Select);
-      let r4 = q4(q4Select);
-      let r5 = q5(q5Select);
+      let r1 = q1();
+      let r2 = q2();
+      let r3 = q3();
+      let r4 = q4();
+      let r5 = q5();
       let allr = r1 + r2 + r3 + r4 + r5;
       let result = allr;
       if (result < 0) {
